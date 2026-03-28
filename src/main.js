e82323580ed5b8cada12f5cef6e4df68620e0a27
Lendl63 <212922348+Lendl63@users.noreply.github.com>
2026-03-28 07:22:05 +0100

    feat(css): integrate tailwind via main.css and cleanup file

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './css/output.css' //Import of style CSS file
+import './main.css' //Import of style CSS file
 
 createApp(App).mount('#app')
