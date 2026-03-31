4251ac5b21198ef81653940c85d135a2067bcfb7
Lendl63 <212922348+Lendl63@users.noreply.github.com>
2026-03-31 22:15:19 +0100

    feat: add menu data

---
+++ src/data/dishes.js
@@ -0,0 +1,26 @@
+export const dishes = [
+    {
+        name: 'Lumpia with Suace',
+        description: 'Lorem ipsum dolor sit , consectetur adipiscing elit, sed do eiusmod tempor ',
+        price: '$12',
+        image: '/src/assets/dish-1 1.jpg'
+    },
+    {
+        name: 'Fish and Veggie',
+        description: 'Lorem ipsum dolor sit , consectetur adipiscing elit, sed do eiusmod tempor ',
+        price: '$12',
+        image: '/src/assets/dish-3 1.jpg'
+    },
+    {
+        name: 'Tofu Chili',
+        description: 'Lorem ipsum dolor sit , consectetur adipiscing elit, sed do eiusmod tempor ',
+        price: '$12',
+        image: '/src/assets/dish-5.jpg'
+    },
+    {
+        name: 'Egg and Cocumber',
+        description: 'Lorem ipsum dolor sit , consectetur adipiscing elit, sed do eiusmod tempor ',
+        price: '$12',
+        image: '/src/assets/dish-4.jpg'
+    },
+]
