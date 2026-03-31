13c17d5be10eb8ee6838f3b1511ec819adb1b8b9
Lendl63 <212922348+Lendl63@users.noreply.github.com>
2026-03-31 22:16:54 +0100

    feat: implemnt responsive headr component

---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header class="flex items-center justify-around p-4">
+        <div class="flex items-center gap-1">
+            <Logo/>
+            <h2 class="text-2xl font-bold p-2 mt-4">restaurant</h2>
+        </div>
+        <nav class="flex items-center gap-8 mt-4 text-lg">
+            <a href="">Menu</a>
+            <a href="">Events</a>
+            <a href="">Gallery</a>
+            <a href="">About</a>
+            <a href="">Contact</a>
+        </nav>
+        <button class="text-lg text-white font-bold p-4 mt-4 bg-primary rounded-btn shadow-2xl">Book a table</button>
+    </header>
+
+</template>
+
+<script setup>
+    import Logo from '/src/assets/Group 13.svg?component'
+
+</script>
