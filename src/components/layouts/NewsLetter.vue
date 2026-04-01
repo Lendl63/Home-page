a5bf79a6be763b0debf7bc990014a7928f0bf608
Lendl63 <212922348+Lendl63@users.noreply.github.com>
2026-04-01 23:35:01 +0100

    feat: add newsletter subscription banner and form

---
+++ src/components/layouts/NewsLetter.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex flex-col items-center py-8 w-4/5 rounded-3xl bg-[url('/src/assets/cta-img-bg1.jpg')] bg-cover bg-no-repeat bg-center">
+        <h2 class="text-5xl text-white text-center font-bold px-2 pt-14 mb-5">Get Or Promo Code by <br>Subscribing To our Newsletter</h2>
+        <form class="flex items-center justify-between gap-5 py-3 mt-7 bg-white rounded-xl w-1/2">
+            <input type="email" name="email" id="email" placeholder="Enter your email" required class="py-3 px-4 bg-white text-lg text-black w-2/3 focus:outline-0">
+            <button class="bg-primary p-3 text-lg text-white font-bold rounded-lg mr-4">Subscribe</button>
+        </form>
+    </div>
+</template>
+
+<script setup>
+</script>
