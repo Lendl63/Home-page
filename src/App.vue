5fca983d32edd8cbf9ef5500f6334e025836de16
Lendl63 <212922348+Lendl63@users.noreply.github.com>
2026-04-01 23:38:56 +0100

    feat: assemble sections in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
-    <div class="m-10">
-        bloc de texte
-    </div>
+    <Header/>
+    <Hero/>
+    <Dishes/>
+    <Chef/>
 </template>
 
  <script setup>
+import Header from './components/layouts/Header.vue';
+import Chef from './components/sections/Chef.vue';
+import Dishes from './components/sections/Dishes.vue';
+import Hero from './components/sections/Hero.vue';
+
 </script>
