af0f0d87ca9e0bf9b4865cda7ac0441575362303
Lendl63 <212922348+Lendl63@users.noreply.github.com>
2026-04-12 20:04:28 +0100

    feat: implement main app

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
     <Header/>
-    <Hero/>
-    <Dishes/>
-    <Chef/>
+    <main>
+        <Hero/>
+        <Dishes/>
+        <Chef/>
+        <Testimonials/>
+        <NewsLetter/>
+    </main>
+    <Footer/>
 </template>
 
  <script setup>
+import Footer from './components/layouts/Footer.vue';
 import Header from './components/layouts/Header.vue';
 import Chef from './components/sections/Chef.vue';
 import Dishes from './components/sections/Dishes.vue';
 import Hero from './components/sections/Hero.vue';
+import Testimonials from './components/sections/Testimonials.vue';
+import NewsLetter from './components/layouts/NewsLetter.vue';
 
 </script>
